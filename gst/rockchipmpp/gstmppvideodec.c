8fa0ce7de7b58eaecea4dc0474745d65b7759ff7
Jacob Chen <jacob-chen@iotwrt.com>
2017-08-23 13:14:40 +0800

    Revert "HACK: make vlc rtsp work"
    This reverts commit 25419fb206267037f620b78780e4b3850038e52d.

--- gst/rockchipmpp/gstmppvideodec.c
+++ gst/rockchipmpp/gstmppvideodec.c
@@ -553,12 +553,6 @@ gst_mpp_video_dec_handle_frame (GstVideoDecoder * decoder,
     self->mpi->control (self->mpp_ctx, MPP_SET_OUTPUT_BLOCK_TIMEOUT,
         (gpointer) & block_timeout);
 
-    self->first_frame = 0;
-
-    GST_VIDEO_DECODER_STREAM_LOCK (decoder);
-  }
-
-  if(!self->first_frame) {
     if (gst_mpp_video_acquire_frame_format (self)) {
       GstVideoCodecState *output_state;
       GstVideoInfo *info = &self->info;
@@ -583,10 +577,10 @@ gst_mpp_video_dec_handle_frame (GstVideoDecoder * decoder,
         goto error_activate_pool;
 
       self->mpi->control (self->mpp_ctx, MPP_DEC_SET_INFO_CHANGE_READY, NULL);
-      self->first_frame = 1;
     } else {
-      goto drop;
+      goto not_negotiated;
     }
+    GST_VIDEO_DECODER_STREAM_LOCK (decoder);
   }
 
   /* Start the output thread if it is not started before */
